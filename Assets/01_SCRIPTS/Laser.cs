e4becee2a65b24912db536b5303417b12c03ea45
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2021-04-24 15:53:20 +0200

    added fuel warning, shorten laser length

--- Assets/01_SCRIPTS/Laser.cs
+++ Assets/01_SCRIPTS/Laser.cs
@@ -11,12 +11,12 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(laserTransform.position, laserTransform.up * -20, out RaycastHit hit))
+        if (Physics.Raycast(laserTransform.position, laserTransform.up * -15, out RaycastHit hit))
         {
             //hit.point
-            Debug.DrawRay(laserTransform.position, laserTransform.up * -20, Color.red, 0.5f);
+            Debug.DrawRay(laserTransform.position, laserTransform.up * -15, Color.red, 0.5f);
             contactPos = hit.point;
-            float size = Mathf.Clamp(Vector3.Distance(laserTransform.position, contactPos), 0, 20);
+            float size = Mathf.Clamp(Vector3.Distance(laserTransform.position, contactPos), 0, 15);
             laserTransform.localScale = new Vector3(laserTransform.localScale.x, size, laserTransform.localScale.z);
 
             hitObject = hit.transform.gameObject;
@@ -29,7 +29,7 @@ public class Laser : MonoBehaviour
         else
         {
             hitObject = null;
-            laserTransform.localScale = new Vector3(laserTransform.localScale.x, 20, laserTransform.localScale.z);
+            laserTransform.localScale = new Vector3(laserTransform.localScale.x, 15, laserTransform.localScale.z);
         }
 
     }
