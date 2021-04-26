4557f12985ed569a3ec72d877af4eb136aca0384
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2021-04-26 20:11:19 +0200

    Patrick

--- Assets/01_SCRIPTS/PlayerLife.cs
+++ Assets/01_SCRIPTS/PlayerLife.cs
@@ -80,6 +80,16 @@ public class PlayerLife : MonoBehaviour
                 ObjectImpact.clip = RockImpacts[Random.Range(0, RockImpacts.Length)];
                 ObjectImpact.Play();
             }
+            if (collision.transform.tag == "Patrick")
+            {
+                int r2 = Random.Range(0, 5);
+                if (r2 == 0) { ObjectImpact.clip = FungusImpactClips[1]; }
+                else { ObjectImpact.clip = FungusImpactClips[0]; }
+                ObjectImpact.Play();
+
+                HPs[life - 1].enabled = false;
+                life -= 1;
+            }
         }
 
         if (life <= 0)
